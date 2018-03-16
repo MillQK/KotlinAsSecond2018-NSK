2eeedb173053d4c7c4bd4b841d61fab4cc94ee1a
Nikita Nikolenko <nikit011418@gmail.com>
2018-03-16 22:44:08 +0700

    lesson7 task1 fix

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,32 +38,93 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <= 0 || width <= 0) throw IllegalArgumentException("Incorrect height: $height or width: $width")
+
+    return object : Matrix<E> {
+        val elems = MutableList(height * width) { e }
+
+        override val height: Int
+            get() = height
+
+        override val width: Int
+            get() = width
+
+        private fun toPosition(row: Int, column: Int): Int = row * width + column
+        private fun checkAndGetPosition(row: Int, column: Int) : Int {
+            val position = toPosition(row, column)
+            if (position !in elems.indices) throw IndexOutOfBoundsException("Wrong row or column")
+            return position
+        }
+
+        override fun get(row: Int, column: Int): E {
+            return elems[checkAndGetPosition(row, column)]
+        }
+
+        override fun get(cell: Cell): E = get(cell.row, cell.column)
+
+        override fun set(row: Int, column: Int, value: E) {
+            elems[checkAndGetPosition(row, column)] = value
+        }
+
+        override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+    }
+}
+
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, init: (Int) -> E) : Matrix<E> {
+    val elems = MutableList(height * width, init)
 
-    override val width: Int = TODO()
+    private fun toPosition(row: Int, column: Int): Int = row * width + column
+    private fun checkAndGetPosition(row: Int, column: Int) : Int {
+        val position = toPosition(row, column)
+        if (position !in elems.indices) throw IndexOutOfBoundsException("Wrong row or column")
+        return position
+    }
 
-    override fun get(row: Int, column: Int): E  = TODO()
+    override fun get(row: Int, column: Int): E {
+        return elems[checkAndGetPosition(row, column)]
+    }
 
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(cell: Cell): E = get(cell.row, cell.column)
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        elems[checkAndGetPosition(row, column)] = value
     }
 
-    override fun set(cell: Cell, value: E) {
-        TODO()
+    override fun set(cell: Cell, value: E) = set(cell.row, cell.column, value)
+
+    override fun equals(other: Any?): Boolean {
+        if (other == this) return true
+        if (other !is Matrix<*>) return false
+
+        if ((height != other.height) || (width != other.width)) return false
+
+        for (row in 0 until height) {
+            for (column in 0 until width) {
+                val thisElem = this[row, column]
+                val otherElem = other[row, column]
+                if (!(thisElem?.equals(other) ?: (otherElem == null))) {
+                    return false
+                }
+            }
+        }
+
+        return true
     }
 
-    override fun equals(other: Any?) = TODO()
-
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        for (i in 0 until height) {
+            elems.subList(i * width, (i + 1) * width).joinTo(sb, separator = " ", prefix = "[", postfix = "]")
+            sb.appendln()
+        }
+        return sb.toString()
+    }
 }
 
